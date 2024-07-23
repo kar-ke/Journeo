e3cb4a81b8c18b1e002ded19e051b5f96fc83c28
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2024-07-23 13:38:39 +0530

    navbar finished - responsively

---
+++ script/script.js
@@ -0,0 +1,7 @@
+
+// Navbar functions 
+function openSearchbar() { document.querySelector('.search-bar').classList.add('open') };
+function closeSearchBar() { document.querySelector('.search-bar').classList.remove('open') };
+
+function openMainNav() { document.querySelector('.main-nav').classList.add('open') };
+function closeMainNav() { document.querySelector('.main-nav').classList.remove('open') };
