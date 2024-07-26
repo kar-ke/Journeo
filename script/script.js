75dc17a1451cd8a35d56bb6d1754961e38611c37
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2024-07-26 10:14:12 +0530

    Animations done

--- script/script.js
+++ script/script.js
@@ -5,3 +5,49 @@ function closeSearchBar() { document.querySelector('.search-bar').classList.remo
 
 function openMainNav() { document.querySelector('.main-nav').classList.add('open') };
 function closeMainNav() { document.querySelector('.main-nav').classList.remove('open') };
+
+// scroll animation
+const article = document.getElementById('article');
+const quote = document.getElementById('quote1');
+const features = document.getElementById('features-1');
+const places = document.getElementById('places-grid');
+const otherFeatures = document.getElementById('other-features-section');
+const blogs = document.getElementById('blogs-section');
+
+window.onscroll = function () {
+    documentTopVal = window.scrollY;
+    let minusVal = 700;
+    let articleTopVal = article.offsetTop;
+    let quoteTopVal = quote.offsetTop;
+    let featuresTopVal = features.offsetTop;
+    let placesTopVal = places.offsetTop;
+    let otherFeaturesTopVal = otherFeatures.offsetTop;
+    let blogsTopVal = blogs.offsetTop;
+
+
+    if (documentTopVal > articleTopVal - minusVal) {
+        sectionAnim(article, articleTopVal, "lr_anime");
+    }
+    if (documentTopVal > quoteTopVal - minusVal) {
+        sectionAnim(quote, quoteTopVal, "bottom"); 
+    }
+    if (documentTopVal > featuresTopVal - minusVal) {
+        sectionAnim(features, featuresTopVal, "lr_anime");
+    }
+    if (documentTopVal > placesTopVal - minusVal) {
+        sectionAnim(places, placesTopVal, "bottom");
+    }
+    if (documentTopVal > otherFeaturesTopVal - minusVal) {
+        sectionAnim(otherFeatures, otherFeaturesTopVal, "lr_anime");
+    }
+    if (documentTopVal > blogsTopVal - minusVal) {
+        sectionAnim(blogs, blogsTopVal, "bottom");
+    }
+};
+
+function sectionAnim(section, sectionTopVal, bottom_anim) {
+    if (documentTopVal > sectionTopVal - 600) {
+        section.classList.add(bottom_anim);
+    }
+};
+
